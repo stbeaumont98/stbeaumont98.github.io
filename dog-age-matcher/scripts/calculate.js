fb059c6386a7d07da5f77d8ec547116d84d5c202
Spencer Beaumont <spence.beaumont@gmail.com>
2025-05-08 22:44:43 -0400

    Changing the way the dog's birthday is calculated to be more scientifically accurate.

--- dog-age-matcher/scripts/calculate.js
+++ dog-age-matcher/scripts/calculate.js
@@ -1,3 +1,8 @@
+const smallYears = [15, 9, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4];
+const medYears = [15, 9, 4, 4, 4, 6, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4];
+const largeYears = [15, 9, 4, 4, 4, 9, 5, 5, 6, 5, 6, 5, 5, 6, 5, 6];
+const giantYears = [12, 10, 9, 7, 7, 4, 7, 8, 7, 8, 7, 7, 7, 7, 7, 7];
+
 let mDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 function numLY(date) {
@@ -9,6 +14,17 @@ function numLY(date) {
         year--;
 
     return Math.floor(year / 4) - Math.floor(year / 100) + Math.floor(year / 400); 
+}
+
+function countDays(y, m , d) {
+    let n = y * 365 + d;
+
+    for (let i = 0; i < m - 1; i++)
+        n += mDays[i];
+
+    n += numLY(date);
+
+    return n;
 }
 
 function getDiff(date1, date2) {
@@ -34,25 +50,46 @@ function getDiff(date1, date2) {
     return n1 - n2;
 }
 
+// Algorithm: https://stackoverflow.com/a/454794
+function getAge(date1, date2) {
+    const date1Array = date1.split("-");
+    let y1 = parseInt(date1Array[0]), m1 = parseInt(date1Array[1]), d1 = parseInt(date1Array[2]);
+    const date2Array = date2.split("-");
+    let y2 = parseInt(date2Array[0]), m2 = parseInt(date2Array[1]), d2 = parseInt(date2Array[2]);
+
+    let yl = y2, ml = m2;
+    if (m2 < m1 || m2 == m1 && d2 < d1)
+        yl--;
+    if (d2 < d1)
+        ml--;
+    let years = yl - y1;
+    let months = ml + 12 * (y2 - yl) - m1;
+    yl = y2;
+    if (ml == 0) {
+        ml = 12;
+        yl--;
+    }
+    let days = countDays(y2, m2, d2) - countDays(yl, ml, d1);
+
+    return [years, months, days];
+}
+
 function addDays(date, days) {
     var result = new Date(date);
     result.setDate(result.getDate() + days);
     return result;
 }
 
-function calcMatch(dogBirthday, userBirthday) {
+function calcMatch() {
+    let dogBirthday = document.getElementById("dogs-birthday").value;
+    let userBirthday = document.getElementById("user-birthday").value;
     let offset = getDiff(dogBirthday, userBirthday);
+
+    let age = getAge(userBirthday, dogBirthday);
     
     let i = 0;
     while (i * 7 < offset + i)
         i++;
 
-    return addDays(dogBirthday, i);
-}
-
-function submitForm() {
-    let dogBirthday = document.getElementById("dogs-birthday").value;
-    let userBirthday = document.getElementById("user-birthday").value;
-
-    document.getElementById("result").innerHTML = calcMatch(dogBirthday, userBirthday);
+    document.getElementById("result").innerHTML = age.toString();//addDays(dogBirthday, i).toDateString();
 }
